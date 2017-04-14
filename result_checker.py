1ce5f251b965e5472a5877e313142ab592c0b7e0
Chadys <chadys@hotmail.fr>
2017-04-15 01:34:37 +0200

    parsing .out

--- result_checker.py
+++ result_checker.py
@@ -4,8 +4,9 @@ import re
 class Etudiant(object):
     def __init__(self, name):
         self.name = name
-        self.eta_ranking = []
         self.rank = 0
+        self.eta_ranking = {}
+        self.establishment = None
 
     def __repr__(self):
         return '{}'.format(self.name)
@@ -18,6 +19,7 @@ class Establishment(object):
     def __init__(self, name, capacity=0):
         self.name = name
         self.capacity = capacity
+        self.students = []
 
     def __repr__(self):
         return '{}'.format(self.name)
@@ -25,28 +27,32 @@ class Establishment(object):
     def __str__(self):
         return '"'+self.__repr__()+'"'
 
+
 def get_names(set_name, Obj_type):
-    names_list = re.match(r'set '+set_name+' := "(?P<names>.*)";', source.readline())
+    r_exp = re.compile(r'set '+set_name+' := "(?P<names>.*)"\s?;')
+    names_list = re.match(r_exp, source.readline())
     while names_list is None:
-        names_list = re.match(r'set ' + set_name + ' := "(?P<names>.*)";', source.readline())
+        names_list = re.match(r_exp, source.readline())
     names_list = re.split(r'" "', names_list.groupdict().get('names'))
     return [Obj_type(name) for name in names_list]
 
 
 def set_secondary_attr(param_name, attr_name, obj_list):
-    new_attr = re.match(r'param '+param_name+' := "(?P<obj_name>.*)"\s+(?P<attr>\d+)',  source.readline())
+    r_exp = re.compile(r'param '+param_name+' :=\s+"(?P<obj_name>.*)"\s+(?P<attr>\d+)')
+    new_attr = re.match(r_exp,  source.readline())
     while new_attr is None:
-        new_attr = re.match(r'param '+param_name+' := "(?P<obj_name>.*)"\s+(?P<attr>\d+)', source.readline())
+        new_attr = re.match(r_exp, source.readline())
     attr_list = [new_attr]
-    new_attr = re.match(r'\s+"(?P<obj_name>.*)"\s+(?P<attr>\d+)', source.readline())
+    r_exp = re.compile(r'\s+"(?P<obj_name>.*)"\s+(?P<attr>\d+)')
+    new_attr = re.match(r_exp, source.readline())
     while new_attr is not None:
         attr_list.append(new_attr)
-        new_attr = re.match(r'\s+"(?P<obj_name>.*)"\s+(?P<attr>\d+)', source.readline())
+        new_attr = re.match(r_exp, source.readline())
     for o, a in zip(obj_list, attr_list):
         setattr(o, attr_name, int(a.groupdict().get('attr')))
 
 
-with open('test.dat') as source:
+with open('projet1.dat') as source:
     # Get students name list
     students = get_names('ETU', Etudiant)
 
@@ -55,8 +61,42 @@ with open('test.dat') as source:
 
     # Get establishments' capacities
     set_secondary_attr('capa', 'capacity', etas)
-    print([o.capacity for o in etas])
 
     # Get students' ranks
     set_secondary_attr('rank_etu', 'rank', students)
-    print([o.rank for o in students])
+
+    # Get students' ranking by establishment
+    while re.match(r'param rank_eta :', source.readline()) is None:
+        pass
+    reg = re.compile('\s+"(?P<stu_name>.*)"(?P<all_ranks>(?:\s+-?\d+)+)')
+    for s in students:
+        ranks = re.match(reg, source.readline())
+        for r, eta in zip(ranks.groupdict().get('all_ranks').split(), etas):
+            s.eta_ranking[eta] = int(r)
+
+
+with open('projet1.out') as source:
+    source.readline()
+    source.readline()
+
+    # Get students' allotment
+    reg = re.compile(r'^\s+(.+)\n')
+    for stu in students:
+        source.readline()
+        eta_name = re.sub(reg, r'\1', source.readline())
+        stu.establishment = (e for e in etas if e.name == eta_name)
+
+    source.readline()
+    source.readline()
+    source.readline()
+    source.readline()
+
+    # Get establishments' students
+    reg = re.compile(r'^\t((?:[^\t\n]+\t?)*)\n')
+    for eta in etas:
+        source.readline()
+        try:
+            eta.students = list(map(lambda s_name: next(s for s in students if s.name == s_name),
+                                re.findall(reg, source.readline())[0].split('\t')))
+        except IndexError:
+            pass
